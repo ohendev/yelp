495a9f1b21f5625ac8d75085c1d6b8d9dfbdce7d
Laurent VIGNERON <laur.vigneron@gmail.com>
2021-12-10 13:59:42 -0400

    fixed issue with reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    raise
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
